a86607a348afcfb977da9df13adfd312d478ed12
Noak-Sergey <noakstuff@gmail.com>
2022-07-04 12:27:31 +0300

    first commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,17 @@
+import './app-info.css';
+
+const AppInfo = (props) => {
+
+    const{allEmployees, increased} = props;
+
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании N</h1>
+            <h2>Общее число сотрудников: {allEmployees}</h2>
+            <h2>Премию получат: {increased} </h2>
+
+        </div>
+    )
+}
+
+export default AppInfo;
